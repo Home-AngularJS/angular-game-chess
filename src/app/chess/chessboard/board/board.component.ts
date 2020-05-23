255c79ee9dee71493825cd39be73d2204b1f7a5f
Aleksandr Lazarchuk <aleksandr.lazarchuk@escalibre.net>
2020-05-23 03:06:59 +0300

    #dev  added: style move chess piece

--- src/app/chess/chessboard/board/board.component.ts
+++ src/app/chess/chessboard/board/board.component.ts
@@ -12,6 +12,39 @@ export class BoardComponent implements OnInit {
     .fill(0)
     .map((_, i) => i)
 
+  isChessBlackKingPosition = false
+  isChessBlackQueenPosition = false
+  isChessBlackBishop1Position = false
+  isChessBlackBishop2Position = false
+  isChessBlackKnight1Position = false
+  isChessBlackKnight2Position = false
+  isChessBlackRook1Position = false
+  isChessBlackRook2Position = false
+  isChessBlackPawn1Position = false
+  isChessBlackPawn2Position = false
+  isChessBlackPawn3Position = false
+  isChessBlackPawn4Position = false
+  isChessBlackPawn5Position = false
+  isChessBlackPawn6Position = false
+  isChessBlackPawn7Position = false
+  isChessBlackPawn8Position = false
+  isChessWhitePawn1Position = false
+  isChessWhitePawn2Position = false
+  isChessWhitePawn3Position = false
+  isChessWhitePawn4Position = false
+  isChessWhitePawn5Position = false
+  isChessWhitePawn6Position = false
+  isChessWhitePawn7Position = false
+  isChessWhitePawn8Position = false
+  isChessWhiteKingPosition = false
+  isChessWhiteQueenPosition = false
+  isChessWhiteBishop1Position = false
+  isChessWhiteBishop2Position = false
+  isChessWhiteKnight1Position = false
+  isChessWhiteKnight2Position = false
+  isChessWhiteRook1Position = false
+  isChessWhiteRook2Position = false
+
   constructor(private chessService : ChessService) {
   }
 
@@ -19,7 +52,7 @@ export class BoardComponent implements OnInit {
   }
 
   position(pos): Position {
-    console.log("position:")
+    // console.log("position:")
     return {
         x: pos % 8,
         y: Math.floor(pos / 8)
@@ -31,42 +64,77 @@ export class BoardComponent implements OnInit {
   }
 
   isBlackSquare({ x, y }: Position) {
-    console.log('X=' + x + ' Y=' + y)
+    // console.log('X=' + x + ' Y=' + y)
     return (x + y) % 2 === 1
   }
 
   handleSquare(position: Position) {
-    this.chessService.moveChessBlackKing(position);
-    this.chessService.moveChessBlackQueen(position);
-    this.chessService.moveChessBlackBishop1(position);
-    this.chessService.moveChessBlackBishop2(position);
-    this.chessService.moveChessBlackKnight1(position);
-    this.chessService.moveChessBlackKnight2(position);
-    this.chessService.moveChessBlackRook1(position);
-    this.chessService.moveChessBlackRook2(position);
-    this.chessService.moveChessBlackPawn1(position);
-    this.chessService.moveChessBlackPawn2(position);
-    this.chessService.moveChessBlackPawn3(position);
-    this.chessService.moveChessBlackPawn4(position);
-    this.chessService.moveChessBlackPawn5(position);
-    this.chessService.moveChessBlackPawn6(position);
-    this.chessService.moveChessBlackPawn7(position);
-    this.chessService.moveChessBlackPawn8(position);
-    this.chessService.moveChessWhitePawn1(position);
-    this.chessService.moveChessWhitePawn2(position);
-    this.chessService.moveChessWhitePawn3(position);
-    this.chessService.moveChessWhitePawn4(position);
-    this.chessService.moveChessWhitePawn5(position);
-    this.chessService.moveChessWhitePawn6(position);
-    this.chessService.moveChessWhitePawn7(position);
-    this.chessService.moveChessWhitePawn8(position);
-    this.chessService.moveChessWhiteKing(position);
-    this.chessService.moveChessWhiteQueen(position);
-    this.chessService.moveChessWhiteBishop1(position);
-    this.chessService.moveChessWhiteBishop2(position);
-    this.chessService.moveChessWhiteKnight1(position);
-    this.chessService.moveChessWhiteKnight2(position);
-    this.chessService.moveChessWhiteRook1(position);
-    this.chessService.moveChessWhiteRook2(position);
+    if (this.isChessBlackRook1Position) this.chessService.moveChessBlackRook1(position)
+    if (this.isChessBlackKnight1Position) this.chessService.moveChessBlackKnight1(position)
+    if (this.isChessBlackBishop1Position) this.chessService.moveChessBlackBishop1(position)
+    if (this.isChessBlackQueenPosition) this.chessService.moveChessBlackQueen(position)
+    if (this.isChessBlackKingPosition) this.chessService.moveChessBlackKing(position)
+    if (this.isChessBlackBishop2Position) this.chessService.moveChessBlackBishop2(position)
+    if (this.isChessBlackKnight2Position) this.chessService.moveChessBlackKnight2(position)
+    if (this.isChessBlackRook2Position) this.chessService.moveChessBlackRook2(position)
+    if (this.isChessBlackPawn1Position) this.chessService.moveChessBlackPawn1(position)
+    if (this.isChessBlackPawn2Position) this.chessService.moveChessBlackPawn2(position)
+    if (this.isChessBlackPawn3Position) this.chessService.moveChessBlackPawn3(position)
+    if (this.isChessBlackPawn4Position) this.chessService.moveChessBlackPawn4(position)
+    if (this.isChessBlackPawn5Position) this.chessService.moveChessBlackPawn5(position)
+    if (this.isChessBlackPawn6Position) this.chessService.moveChessBlackPawn6(position)
+    if (this.isChessBlackPawn7Position) this.chessService.moveChessBlackPawn7(position)
+    if (this.isChessBlackPawn8Position) this.chessService.moveChessBlackPawn8(position)
+    if (this.isChessWhitePawn1Position) this.chessService.moveChessWhitePawn1(position)
+    if (this.isChessWhitePawn2Position) this.chessService.moveChessWhitePawn2(position)
+    if (this.isChessWhitePawn3Position) this.chessService.moveChessWhitePawn3(position)
+    if (this.isChessWhitePawn4Position) this.chessService.moveChessWhitePawn4(position)
+    if (this.isChessWhitePawn5Position) this.chessService.moveChessWhitePawn5(position)
+    if (this.isChessWhitePawn6Position) this.chessService.moveChessWhitePawn6(position)
+    if (this.isChessWhitePawn7Position) this.chessService.moveChessWhitePawn7(position)
+    if (this.isChessWhitePawn8Position) this.chessService.moveChessWhitePawn8(position)
+    if (this.isChessWhiteRook1Position) this.chessService.moveChessWhiteRook1(position)
+    if (this.isChessWhiteKnight1Position) this.chessService.moveChessWhiteKnight1(position)
+    if (this.isChessWhiteBishop1Position) this.chessService.moveChessWhiteBishop1(position)
+    if (this.isChessWhiteQueenPosition) this.chessService.moveChessWhiteQueen(position)
+    if (this.isChessWhiteKingPosition) this.chessService.moveChessWhiteKing(position)
+    if (this.isChessWhiteBishop2Position) this.chessService.moveChessWhiteBishop2(position)
+    if (this.isChessWhiteKnight2Position) this.chessService.moveChessWhiteKnight2(position)
+    if (this.isChessWhiteRook2Position) this.chessService.moveChessWhiteRook2(position)
+  }
+
+  handleChess(position: Position) {
+    this.isChessBlackKingPosition = this.equalsPosition(position, this.chessService.chessBlackKingPosition$.value)
+    this.isChessBlackQueenPosition = this.equalsPosition(position, this.chessService.chessBlackQueenPosition$.value)
+    this.isChessBlackBishop1Position = this.equalsPosition(position, this.chessService.chessBlackBishop1Position$.value)
+    this.isChessBlackBishop2Position = this.equalsPosition(position, this.chessService.chessBlackBishop2Position$.value)
+    this.isChessBlackKnight1Position = this.equalsPosition(position, this.chessService.chessBlackKnight1Position$.value)
+    this.isChessBlackKnight2Position = this.equalsPosition(position, this.chessService.chessBlackKnight2Position$.value)
+    this.isChessBlackRook1Position = this.equalsPosition(position, this.chessService.chessBlackRook1Position$.value)
+    this.isChessBlackRook2Position = this.equalsPosition(position, this.chessService.chessBlackRook2Position$.value)
+    this.isChessBlackPawn1Position = this.equalsPosition(position, this.chessService.chessBlackPawn1Position$.value)
+    this.isChessBlackPawn2Position = this.equalsPosition(position, this.chessService.chessBlackPawn2Position$.value)
+    this.isChessBlackPawn3Position = this.equalsPosition(position, this.chessService.chessBlackPawn3Position$.value)
+    this.isChessBlackPawn4Position = this.equalsPosition(position, this.chessService.chessBlackPawn4Position$.value)
+    this.isChessBlackPawn5Position = this.equalsPosition(position, this.chessService.chessBlackPawn5Position$.value)
+    this.isChessBlackPawn6Position = this.equalsPosition(position, this.chessService.chessBlackPawn6Position$.value)
+    this.isChessBlackPawn7Position = this.equalsPosition(position, this.chessService.chessBlackPawn7Position$.value)
+    this.isChessBlackPawn8Position = this.equalsPosition(position, this.chessService.chessBlackPawn8Position$.value)
+    this.isChessWhitePawn1Position = this.equalsPosition(position, this.chessService.chessWhitePawn1Position$.value)
+    this.isChessWhitePawn2Position = this.equalsPosition(position, this.chessService.chessWhitePawn2Position$.value)
+    this.isChessWhitePawn3Position = this.equalsPosition(position, this.chessService.chessWhitePawn3Position$.value)
+    this.isChessWhitePawn4Position = this.equalsPosition(position, this.chessService.chessWhitePawn4Position$.value)
+    this.isChessWhitePawn5Position = this.equalsPosition(position, this.chessService.chessWhitePawn5Position$.value)
+    this.isChessWhitePawn6Position = this.equalsPosition(position, this.chessService.chessWhitePawn6Position$.value)
+    this.isChessWhitePawn7Position = this.equalsPosition(position, this.chessService.chessWhitePawn7Position$.value)
+    this.isChessWhitePawn8Position = this.equalsPosition(position, this.chessService.chessWhitePawn8Position$.value)
+    this.isChessWhiteKingPosition = this.equalsPosition(position, this.chessService.chessWhiteKingPosition$.value)
+    this.isChessWhiteQueenPosition = this.equalsPosition(position, this.chessService.chessWhiteQueenPosition$.value)
+    this.isChessWhiteBishop1Position = this.equalsPosition(position, this.chessService.chessWhiteBishop1Position$.value)
+    this.isChessWhiteBishop2Position = this.equalsPosition(position, this.chessService.chessWhiteBishop2Position$.value)
+    this.isChessWhiteKnight1Position = this.equalsPosition(position, this.chessService.chessWhiteKnight1Position$.value)
+    this.isChessWhiteKnight2Position = this.equalsPosition(position, this.chessService.chessWhiteKnight2Position$.value)
+    this.isChessWhiteRook1Position = this.equalsPosition(position, this.chessService.chessWhiteRook1Position$.value)
+    this.isChessWhiteRook2Position = this.equalsPosition(position, this.chessService.chessWhiteRook2Position$.value)
   }
 }
--- src/app/core/service/chess.service.ts
+++ src/app/core/service/chess.service.ts
@@ -4,165 +4,164 @@ import { Position } from '../model/position';
 
 @Injectable()
 export class ChessService {
-
-  chessBlackKingPosition$ = new BehaviorSubject<Position>({ x: 4, y: 0 });
-  chessBlackQueenPosition$ = new BehaviorSubject<Position>({ x: 3, y: 0 });
-  chessBlackBishop1Position$ = new BehaviorSubject<Position>({ x: 2, y: 0 });
-  chessBlackBishop2Position$ = new BehaviorSubject<Position>({ x: 5, y: 0 });
-  chessBlackKnight1Position$ = new BehaviorSubject<Position>({ x: 1, y: 0 });
-  chessBlackKnight2Position$ = new BehaviorSubject<Position>({ x: 6, y: 0 });
-  chessBlackRook1Position$ = new BehaviorSubject<Position>({ x: 0, y: 0 });
-  chessBlackRook2Position$ = new BehaviorSubject<Position>({ x: 7, y: 0 });
-  chessBlackPawn1Position$ = new BehaviorSubject<Position>({ x: 0, y: 1 });
-  chessBlackPawn2Position$ = new BehaviorSubject<Position>({ x: 1, y: 1 });
-  chessBlackPawn3Position$ = new BehaviorSubject<Position>({ x: 2, y: 1 });
-  chessBlackPawn4Position$ = new BehaviorSubject<Position>({ x: 3, y: 1 });
-  chessBlackPawn5Position$ = new BehaviorSubject<Position>({ x: 4, y: 1 });
-  chessBlackPawn6Position$ = new BehaviorSubject<Position>({ x: 5, y: 1 });
-  chessBlackPawn7Position$ = new BehaviorSubject<Position>({ x: 6, y: 1 });
-  chessBlackPawn8Position$ = new BehaviorSubject<Position>({ x: 7, y: 1 });
-  chessWhitePawn1Position$ = new BehaviorSubject<Position>({ x: 0, y: 6 });
-  chessWhitePawn2Position$ = new BehaviorSubject<Position>({ x: 1, y: 6 });
-  chessWhitePawn3Position$ = new BehaviorSubject<Position>({ x: 2, y: 6 });
-  chessWhitePawn4Position$ = new BehaviorSubject<Position>({ x: 3, y: 6 });
-  chessWhitePawn5Position$ = new BehaviorSubject<Position>({ x: 4, y: 6 });
-  chessWhitePawn6Position$ = new BehaviorSubject<Position>({ x: 5, y: 6 });
-  chessWhitePawn7Position$ = new BehaviorSubject<Position>({ x: 6, y: 6 });
-  chessWhitePawn8Position$ = new BehaviorSubject<Position>({ x: 7, y: 6 });
-  chessWhiteKingPosition$ = new BehaviorSubject<Position>({ x: 4, y: 7 });
-  chessWhiteQueenPosition$ = new BehaviorSubject<Position>({ x: 3, y: 7 });
-  chessWhiteBishop1Position$ = new BehaviorSubject<Position>({ x: 2, y: 7 });
-  chessWhiteBishop2Position$ = new BehaviorSubject<Position>({ x: 5, y: 7 });
-  chessWhiteKnight1Position$ = new BehaviorSubject<Position>({ x: 1, y: 7 });
-  chessWhiteKnight2Position$ = new BehaviorSubject<Position>({ x: 6, y: 7 });
-  chessWhiteRook1Position$ = new BehaviorSubject<Position>({ x: 0, y: 7 });
-  chessWhiteRook2Position$ = new BehaviorSubject<Position>({ x: 7, y: 7 });
+  chessBlackKingPosition$ = new BehaviorSubject<Position>({ x: 4, y: 0 })
+  chessBlackQueenPosition$ = new BehaviorSubject<Position>({ x: 3, y: 0 })
+  chessBlackBishop1Position$ = new BehaviorSubject<Position>({ x: 2, y: 0 })
+  chessBlackBishop2Position$ = new BehaviorSubject<Position>({ x: 5, y: 0 })
+  chessBlackKnight1Position$ = new BehaviorSubject<Position>({ x: 1, y: 0 })
+  chessBlackKnight2Position$ = new BehaviorSubject<Position>({ x: 6, y: 0 })
+  chessBlackRook1Position$ = new BehaviorSubject<Position>({ x: 0, y: 0 })
+  chessBlackRook2Position$ = new BehaviorSubject<Position>({ x: 7, y: 0 })
+  chessBlackPawn1Position$ = new BehaviorSubject<Position>({ x: 0, y: 1 })
+  chessBlackPawn2Position$ = new BehaviorSubject<Position>({ x: 1, y: 1 })
+  chessBlackPawn3Position$ = new BehaviorSubject<Position>({ x: 2, y: 1 })
+  chessBlackPawn4Position$ = new BehaviorSubject<Position>({ x: 3, y: 1 })
+  chessBlackPawn5Position$ = new BehaviorSubject<Position>({ x: 4, y: 1 })
+  chessBlackPawn6Position$ = new BehaviorSubject<Position>({ x: 5, y: 1 })
+  chessBlackPawn7Position$ = new BehaviorSubject<Position>({ x: 6, y: 1 })
+  chessBlackPawn8Position$ = new BehaviorSubject<Position>({ x: 7, y: 1 })
+  chessWhitePawn1Position$ = new BehaviorSubject<Position>({ x: 0, y: 6 })
+  chessWhitePawn2Position$ = new BehaviorSubject<Position>({ x: 1, y: 6 })
+  chessWhitePawn3Position$ = new BehaviorSubject<Position>({ x: 2, y: 6 })
+  chessWhitePawn4Position$ = new BehaviorSubject<Position>({ x: 3, y: 6 })
+  chessWhitePawn5Position$ = new BehaviorSubject<Position>({ x: 4, y: 6 })
+  chessWhitePawn6Position$ = new BehaviorSubject<Position>({ x: 5, y: 6 })
+  chessWhitePawn7Position$ = new BehaviorSubject<Position>({ x: 6, y: 6 })
+  chessWhitePawn8Position$ = new BehaviorSubject<Position>({ x: 7, y: 6 })
+  chessWhiteKingPosition$ = new BehaviorSubject<Position>({ x: 4, y: 7 })
+  chessWhiteQueenPosition$ = new BehaviorSubject<Position>({ x: 3, y: 7 })
+  chessWhiteBishop1Position$ = new BehaviorSubject<Position>({ x: 2, y: 7 })
+  chessWhiteBishop2Position$ = new BehaviorSubject<Position>({ x: 5, y: 7 })
+  chessWhiteKnight1Position$ = new BehaviorSubject<Position>({ x: 1, y: 7 })
+  chessWhiteKnight2Position$ = new BehaviorSubject<Position>({ x: 6, y: 7 })
+  chessWhiteRook1Position$ = new BehaviorSubject<Position>({ x: 0, y: 7 })
+  chessWhiteRook2Position$ = new BehaviorSubject<Position>({ x: 7, y: 7 })
 
   moveChessBlackKing(to: Position) {
-    this.chessBlackKingPosition$.next(to);
+    this.chessBlackKingPosition$.next(to)
   }
 
   moveChessBlackQueen(to: Position) {
-    this.chessBlackQueenPosition$.next(to);
+    this.chessBlackQueenPosition$.next(to)
   }
 
   moveChessBlackBishop1(to: Position) {
-    this.chessBlackBishop1Position$.next(to);
+    this.chessBlackBishop1Position$.next(to)
   }
 
   moveChessBlackBishop2(to: Position) {
-    this.chessBlackBishop2Position$.next(to);
+    this.chessBlackBishop2Position$.next(to)
   }
 
   moveChessBlackKnight1(to: Position) {
-    this.chessBlackKnight1Position$.next(to);
+    this.chessBlackKnight1Position$.next(to)
   }
 
   moveChessBlackKnight2(to: Position) {
-    this.chessBlackKnight2Position$.next(to);
+    this.chessBlackKnight2Position$.next(to)
   }
 
   moveChessBlackRook1(to: Position) {
-    this.chessBlackRook1Position$.next(to);
+    this.chessBlackRook1Position$.next(to)
   }
 
   moveChessBlackRook2(to: Position) {
-    this.chessBlackRook2Position$.next(to);
+    this.chessBlackRook2Position$.next(to)
   }
 
   moveChessBlackPawn1(to: Position) {
-    this.chessBlackPawn1Position$.next(to);
+    this.chessBlackPawn1Position$.next(to)
   }
 
   moveChessBlackPawn2(to: Position) {
-    this.chessBlackPawn2Position$.next(to);
+    this.chessBlackPawn2Position$.next(to)
   }
 
   moveChessBlackPawn3(to: Position) {
-    this.chessBlackPawn3Position$.next(to);
+    this.chessBlackPawn3Position$.next(to)
   }
 
   moveChessBlackPawn4(to: Position) {
-    this.chessBlackPawn4Position$.next(to);
+    this.chessBlackPawn4Position$.next(to)
   }
 
   moveChessBlackPawn5(to: Position) {
-    this.chessBlackPawn5Position$.next(to);
+    this.chessBlackPawn5Position$.next(to)
   }
 
   moveChessBlackPawn6(to: Position) {
-    this.chessBlackPawn6Position$.next(to);
+    this.chessBlackPawn6Position$.next(to)
   }
 
   moveChessBlackPawn7(to: Position) {
-    this.chessBlackPawn7Position$.next(to);
+    this.chessBlackPawn7Position$.next(to)
   }
 
   moveChessBlackPawn8(to: Position) {
-    this.chessBlackPawn8Position$.next(to);
+    this.chessBlackPawn8Position$.next(to)
   }
 
   moveChessWhitePawn1(to: Position) {
-    this.chessWhitePawn1Position$.next(to);
+    this.chessWhitePawn1Position$.next(to)
   }
 
   moveChessWhitePawn2(to: Position) {
-    this.chessWhitePawn2Position$.next(to);
+    this.chessWhitePawn2Position$.next(to)
   }
 
   moveChessWhitePawn3(to: Position) {
-    this.chessWhitePawn3Position$.next(to);
+    this.chessWhitePawn3Position$.next(to)
   }
 
   moveChessWhitePawn4(to: Position) {
-    this.chessWhitePawn4Position$.next(to);
+    this.chessWhitePawn4Position$.next(to)
   }
 
   moveChessWhitePawn5(to: Position) {
-    this.chessWhitePawn5Position$.next(to);
+    this.chessWhitePawn5Position$.next(to)
   }
 
   moveChessWhitePawn6(to: Position) {
-    this.chessWhitePawn6Position$.next(to);
+    this.chessWhitePawn6Position$.next(to)
   }
 
   moveChessWhitePawn7(to: Position) {
-    this.chessWhitePawn7Position$.next(to);
+    this.chessWhitePawn7Position$.next(to)
   }
 
   moveChessWhitePawn8(to: Position) {
-    this.chessWhitePawn8Position$.next(to);
+    this.chessWhitePawn8Position$.next(to)
   }
 
   moveChessWhiteKing(to: Position) {
-    this.chessWhiteKingPosition$.next(to);
+    this.chessWhiteKingPosition$.next(to)
   }
 
   moveChessWhiteQueen(to: Position) {
-    this.chessWhiteQueenPosition$.next(to);
+    this.chessWhiteQueenPosition$.next(to)
   }
 
   moveChessWhiteBishop1(to: Position) {
-    this.chessWhiteBishop1Position$.next(to);
+    this.chessWhiteBishop1Position$.next(to)
   }
 
   moveChessWhiteBishop2(to: Position) {
-    this.chessWhiteBishop2Position$.next(to);
+    this.chessWhiteBishop2Position$.next(to)
   }
 
   moveChessWhiteKnight1(to: Position) {
-    this.chessWhiteKnight1Position$.next(to);
+    this.chessWhiteKnight1Position$.next(to)
   }
 
   moveChessWhiteKnight2(to: Position) {
-    this.chessWhiteKnight2Position$.next(to);
+    this.chessWhiteKnight2Position$.next(to)
   }
 
   moveChessWhiteRook1(to: Position) {
-    this.chessWhiteRook1Position$.next(to);
+    this.chessWhiteRook1Position$.next(to)
   }
 
   moveChessWhiteRook2(to: Position) {
-    this.chessWhiteRook2Position$.next(to);
+    this.chessWhiteRook2Position$.next(to)
   }
 }
