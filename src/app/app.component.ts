460441e1745ddc89f6d537bdb0669b363195631b
Aleksandr Lazarchuk <aleksandr.lazarchuk@escalibre.net>
2020-05-24 16:59:32 +0300

    #dev.voice-recognition  2.0.0-SNAPSHOT: refactor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,69 +8,69 @@ declare const annyang: any;
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  voiceActiveSectionDisabled = true;
-  voiceActiveSectionError = false;
-  voiceActiveSectionSuccess = false;
-  voiceActiveSectionListening = false;
-  voiceText: any;
+  voiceActiveSectionDisabled = true
+  voiceActiveSectionError = false
+  voiceActiveSectionSuccess = false
+  voiceActiveSectionListening = false
+  voiceText: any
 
   constructor(private ngZone: NgZone) { }
 
   initializeVoiceRecognitionCallback(): void {
     annyang.addCallback('error', (err) => {
       if (err.error === 'network') {
-        this.voiceText = 'Internet is require';
-        annyang.abort();
-        this.ngZone.run(() => this.voiceActiveSectionSuccess = true);
+        this.voiceText = 'Internet is require'
+        annyang.abort()
+        this.ngZone.run(() => this.voiceActiveSectionSuccess = true)
       } else if (this.voiceText === undefined) {
-        this.ngZone.run(() => this.voiceActiveSectionError = true);
-        annyang.abort();
+        this.ngZone.run(() => this.voiceActiveSectionError = true)
+        annyang.abort()
       }
-    });
+    })
 
     annyang.addCallback('soundstart', (res) => {
-      this.ngZone.run(() => this.voiceActiveSectionListening = true);
-    });
+      this.ngZone.run(() => this.voiceActiveSectionListening = true)
+    })
 
     annyang.addCallback('end', () => {
       if (this.voiceText === undefined) {
-        this.ngZone.run(() => this.voiceActiveSectionError = true);
-        annyang.abort();
+        this.ngZone.run(() => this.voiceActiveSectionError = true)
+        annyang.abort()
       }
-    });
+    })
 
     annyang.addCallback('result', (userSaid) => {
-      this.ngZone.run(() => this.voiceActiveSectionError = false);
-      const queryText: any = userSaid[0];
-      annyang.abort();
-      this.voiceText = queryText;
-      this.ngZone.run(() => this.voiceActiveSectionListening = false);
-      this.ngZone.run(() => this.voiceActiveSectionSuccess = true);
+      this.ngZone.run(() => this.voiceActiveSectionError = false)
+      const queryText: any = userSaid[0]
+      annyang.abort()
+      this.voiceText = queryText
+      this.ngZone.run(() => this.voiceActiveSectionListening = false)
+      this.ngZone.run(() => this.voiceActiveSectionSuccess = true)
     });
   }
 
   startVoiceRecognition(): void {
-    this.voiceActiveSectionDisabled = false;
-    this.voiceActiveSectionError = false;
-    this.voiceActiveSectionSuccess = false;
-    this.voiceText = undefined;
+    this.voiceActiveSectionDisabled = false
+    this.voiceActiveSectionError = false
+    this.voiceActiveSectionSuccess = false
+    this.voiceText = undefined
 
     if (annyang) {
       const commands = {
         'demo-annyang': () => { }
-      };
-      annyang.addCommands(commands);
-      this.initializeVoiceRecognitionCallback();
-      annyang.start({ autoRestart: false });
+      }
+      annyang.addCommands(commands)
+      this.initializeVoiceRecognitionCallback()
+      annyang.start({ autoRestart: false })
     }
   }
 
   closeVoiceRecognition(): void {
-    this.voiceActiveSectionDisabled = true;
-    this.voiceActiveSectionError = false;
-    this.voiceActiveSectionSuccess = false;
-    this.voiceActiveSectionListening = false;
-    this.voiceText = undefined;
-    if (annyang) { annyang.abort(); }
+    this.voiceActiveSectionDisabled = true
+    this.voiceActiveSectionError = false
+    this.voiceActiveSectionSuccess = false
+    this.voiceActiveSectionListening = false
+    this.voiceText = undefined
+    if (annyang) { annyang.abort() }
   }
 }
